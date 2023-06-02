43ceb2908c7f261d9d8275d4d4a339701735f3f8
Passakorn Puttama <54370241+boss4848@users.noreply.github.com>
2023-06-02 20:42:13 +0700

    [ADD] product controller

---
+++ back_end/controllers/productController.js
@@ -0,0 +1,39 @@
+const Product = require('../models/productModel');
+
+exports.getAllProducts = async (req, res) => {
+    try {
+        const products = await Product.find();
+
+        res.status(200).json({
+            status: 'success',
+            results: products.length,
+            data: {
+                products
+            }
+        });
+    }
+    catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err.message
+        });
+    }
+}
+
+exports.createProduct = async (req, res) => {
+    try {
+        const newProduct = await Product.create(req.body);
+
+        res.status(201).json({
+            status: 'success',
+            data: {
+                product: newProduct
+            }
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 'fail',
+            message: err.message
+        });
+    }
+}
