a4a4fb7c6c99dd171bd5c70c6ff3c97a774f5859
Passakorn Puttama <54370241+boss4848@users.noreply.github.com>
2023-06-02 20:42:01 +0700

    [ADD] product route

---
+++ back_end/routes/productRoute.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+//Controllers
+const productController = require('../controllers/productController');
+
+router
+    .route('/')
+    .get(productController.getAllProducts)
+    .post(productController.createProduct);
+
+// router
+//     .route('/:id')
+//     .get(productController.getProduct)
+//     .patch(productController.updateProduct)
+//     .delete(productController.deleteProduct);
+
+module.exports = router;
