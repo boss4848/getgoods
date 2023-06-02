536e47e7968c4c99d7f5f0cd3584389372893369
Passakorn Puttama <54370241+boss4848@users.noreply.github.com>
2023-06-02 20:41:17 +0700

    [ADD] product model

---
+++ back_end/models/productModel.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        required: [true, 'A product must have a name'],
+        trim: true,
+        maxlength: [40, 'A product name must have less or equal then 40 characters'],
+        // minlength: [10, 'A product name must have more or equal then 10 characters']
+    },
+    slug: String,
+    price: {
+        type: Number,
+        required: [true, 'A product must have a price']
+    },
+    description: {
+        type: String,
+        trim: true,
+        required: [true, 'A product must have a description']
+    },
+    category: {
+        type: String,
+        required: [true, 'A product must belong to a category']
+    },
+    quantity: {
+        type: Number,
+        required: [true, 'A product must have a quantity']
+    },
+    sold: {
+        type: Number,
+        default: 0
+    },
+    images: {
+        type: Array,
+        default: []
+    },
+    // shipping: {
+    //     type: String,
+    //     enum: ['Yes', 'No']
+    // },
+    // color: {
+    //     type: String,
+    //     enum: ['Black', 'Brown', 'Silver', 'White', 'Blue']
+    // },
+    // brand: {
+    //     type: String,
+    //     enum: ['Apple', 'Samsung', 'Microsoft', 'Lenovo', 'ASUS']
+    // },
+    ratings: {
+        type: Number,
+        default: 0
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Product', productSchema);
