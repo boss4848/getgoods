7263e1fccab8477fbd9718f54959ebb80af7f00d
Passakorn Puttama <54370241+boss4848@users.noreply.github.com>
2023-06-02 23:51:15 +0700

    [UPDATE] request middleware status

--- back_end/app.js
+++ back_end/app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
+const chalk = require('chalk');
+const morgan = require('morgan');
+
 
 app.use(express.json());
 
+//Middleware
+//Log request method, url, and time
 app.use((req, res, next) => {
-    req.requestTime = new Date().toISOString();
+    console.log(` ${chalk.yellowBright("|")} ${chalk.blue.bold(req.method)} ${req.url} - ${chalk.yellowBright(new Date().toLocaleTimeString())}`);
     next();
 });
+
 
 //Routes
 app.use('/api/v1/products', require('./routes/productRoute'));
