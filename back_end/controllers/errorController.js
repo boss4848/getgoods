07b830ab557aa29fa519900f34bea3e5ab71a21a
Passakorn Puttama <54370241+boss4848@users.noreply.github.com>
2023-06-03 16:34:46 +0700

    [UPDATE] handling JWT error and JWT expired error

--- back_end/controllers/errorController.js
+++ back_end/controllers/errorController.js
@@ -16,7 +16,8 @@ const handleValidationErrorDB = err => {
     const message = `Invalid input data. ${errors.join('. ')}`;
     return new AppError(message, 400);
 }
-
+const handleJWTError = () => new AppError('Invalid token. Please log in again!', 401);
+const handleJWTExpiredError = () => new AppError('Your token has expired! Please log in again.', 401);
 const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
@@ -56,6 +57,8 @@ module.exports = (err, req, res, next) => {
         if (error.name === 'CastError') error = handleCastErrorDB(error);
         if (error.code === 11000) error = handleDuplicateFieldsDB(error);
         if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
+        if (error.name === 'JsonWebTokenError') error = handleJWTError();
+        if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
         sendErrorProd(error, res);
     }
 }
