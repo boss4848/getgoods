7629bc0f9a8e366f0f466313d3ca736b3962a3c0
Passakorn Puttama <54370241+boss4848@users.noreply.github.com>
2023-06-03 13:39:47 +0700

    [ADD] handling invalid IDs

--- back_end/controllers/errorController.js
+++ back_end/controllers/errorController.js
@@ -1,3 +1,9 @@
+const AppError = require('../utils/appError');
+const handleCastErrorDB = err => {
+    const message = `Invalid ${err.path}: ${err.value}.`;
+    return new AppError(message, 400);
+}
+
 const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
@@ -32,6 +38,9 @@ module.exports = (err, req, res, next) => {
     if (process.env.NODE_ENV === 'development') {
         sendErrorDev(err, res);
     } else if (process.env.NODE_ENV === 'production') {
-        sendErrorProd(err, res);
+        let error = { ...err }; //create a copy of err object
+        if (err.name === 'CastError') error = handleCastErrorDB(err);
+
+        sendErrorProd(error, res);
     }
 }
