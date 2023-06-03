758c0f4deb4150534cb4698b0e1b72392047db53
Passakorn Puttama <54370241+boss4848@users.noreply.github.com>
2023-06-03 13:53:48 +0700

    [ADD] unhandled rejections and catching uncaught exceptions

--- back_end/server.js
+++ back_end/server.js
@@ -2,13 +2,18 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
-dotenv.config({ path: './config.env' });
+//Handle uncaught exceptions
+process.on('uncaughtException', err => {
+    console.log(err.name, err.message);
+    console.log('UNCAUGHT EXCEPTION! Shutting down...');
+    process.exit(1);
+});
 
+dotenv.config({ path: './config.env' });
 const DB = process.env.DATABASE.replace(
     '<PASSWORD>',
     process.env.DATABASE_PASSWORD
 );
-
 
 mongoose.connect(DB, {
     useNewUrlParser: true,
@@ -19,6 +24,16 @@ mongoose.connect(DB, {
 });
 
 const port = process.env.PORT || 3000;
-const server = app.listen(port, () => {
+app.listen(port, () => {
     console.log(`App running on port ${port}...`);
 });
+
+//Handle unhandled rejections
+process.on('unhandledRejection', err => {
+    console.log(err.name, err.message);
+    console.log('UNHANDLED REJECTION! Shutting down...');
+    server.close(() => {
+        process.exit(1);
+    });
+});
+
