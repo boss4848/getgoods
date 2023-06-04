1bd02d6443be4a0d2e88ec624a1341e06c882442
Passakorn Puttama <54370241+boss4848@users.noreply.github.com>
2023-06-04 14:55:53 +0700

    [CRAETE] user controller and adding photo upload

---
+++ back_end/controllers/userController.js
@@ -0,0 +1,79 @@
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+const multer = require('multer');
+const sharp = require('sharp');
+// const multerStorage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, 'public/img/users');
+//     },
+//     filename: (req, file, cb) => {
+//         //user-<user id>-<timestamp>.jpeg
+//         const ext = file.mimetype.split('/')[1];
+//         cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+//     }
+// });
+const multerStorage = multer.memoryStorage();
+const multerFilter = (req, file, cb) => {
+    //Only accept image files
+    console.log(file)
+    if (file.mimetype.startsWith('image')) {
+        cb(null, true);
+    } else {
+        cb(new AppError('Not an image! Please upload only images.', 400), false);
+    }
+};
+const upload = multer({
+    storage: multerStorage,
+    fileFilter: multerFilter
+});
+
+exports.uploadUserPhoto = upload.single('photo');
+exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
+    // console.log(req.file);
+    if (!req.file) return next();
+    req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+    // console.log(req.file.filename);
+    await sharp(req.file.buffer)
+        .resize(500, 500)
+        .toFormat('jpeg')
+        .jpeg({
+            quality: 90
+        })
+        .toFile(`public/img/users/${req.file.filename}`);
+    next();
+});
+const filterObj = (obj, ...allowedFields) => {
+    const newObj = {};
+    Object.keys(obj).forEach(el => {
+        if (allowedFields.includes(el)) newObj[el] = obj[el];
+    });
+    return newObj;
+};
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+    // Create error if user POSTs password data
+    if (req.body.password || req.body.passwordConfirm) {
+        return next(
+            new AppError(
+                'This route is not for password updates. Please use /updateMyPassword.',
+                400
+            )
+        );
+    }
+    // Filtered out unwanted fields names that are not allowed to be updated
+    const filteredBody = filterObj(req.body, 'name', 'email');
+    if (req.file) filteredBody.photo = req.file.filename;
+
+    // Update user document
+    const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+        new: true,
+        runValidators: true
+    });
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            user: updatedUser
+        }
+    });
+});
