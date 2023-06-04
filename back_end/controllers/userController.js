4abc399a568df3094ad5311c830e37d4ff349e0a
Passakorn Puttama <54370241+boss4848@users.noreply.github.com>
2023-06-04 16:59:19 +0700

    [FIX] change image storage to the cloud

--- back_end/controllers/userController.js
+++ back_end/controllers/userController.js
@@ -1,47 +1,57 @@
+const { BlobServiceClient } = require('@azure/storage-blob');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const multer = require('multer');
 const sharp = require('sharp');
-// const multerStorage = multer.diskStorage({
-//     destination: (req, file, cb) => {
-//         cb(null, 'public/img/users');
-//     },
-//     filename: (req, file, cb) => {
-//         //user-<user id>-<timestamp>.jpeg
-//         const ext = file.mimetype.split('/')[1];
-//         cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
-//     }
-// });
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
 const multerStorage = multer.memoryStorage();
 const multerFilter = (req, file, cb) => {
     //Only accept image files
-    console.log(file)
+    console.log(file);
     if (file.mimetype.startsWith('image')) {
         cb(null, true);
     } else {
         cb(new AppError('Not an image! Please upload only images.', 400), false);
     }
 };
+
 const upload = multer({
     storage: multerStorage,
     fileFilter: multerFilter
 });
 
+const blobServiceClient = BlobServiceClient.fromConnectionString(process.env.AZURE_STORAGE_CONNECTION_STRING);
+// Create a container if it doesn't exist
+const createContainer = async (containerName) => {
+    const containerClient = blobServiceClient.getContainerClient(containerName);
+    const exists = await containerClient.exists();
+
+    if (!exists) {
+        await containerClient.create();
+    }
+};
+// Call the createContainer function before using the container
+createContainer('user-photos');
 exports.uploadUserPhoto = upload.single('photo');
 exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
-    // console.log(req.file);
     if (!req.file) return next();
     req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
-    // console.log(req.file.filename);
-    await sharp(req.file.buffer)
-        .resize(500, 500)
-        .toFormat('jpeg')
-        .jpeg({
-            quality: 90
-        })
-        .toFile(`public/img/users/${req.file.filename}`);
+
+    // Upload the image to Azure Blob Storage
+    const containerClient = blobServiceClient.getContainerClient('user-photos');
+    const blockBlobClient = containerClient.getBlockBlobClient(req.file.filename);
+
+    await blockBlobClient.uploadData(req.file.buffer, {
+        blobHTTPHeaders: {
+            blobContentType: 'image/jpeg'
+        }
+    });
+
     next();
 });
+
 const filterObj = (obj, ...allowedFields) => {
     const newObj = {};
     Object.keys(obj).forEach(el => {
@@ -60,6 +70,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
             )
         );
     }
+
     // Filtered out unwanted fields names that are not allowed to be updated
     const filteredBody = filterObj(req.body, 'name', 'email');
     if (req.file) filteredBody.photo = req.file.filename;
@@ -74,6 +85,26 @@ exports.updateMe = catchAsync(async (req, res, next) => {
         status: 'success',
         data: {
             user: updatedUser
+        }
+    });
+});
+
+exports.getMe = catchAsync(async (req, res, next) => {
+    // Get the current user
+    const user = await User.findById(req.user.id);
+
+    // Retrieve the file URL from Azure Blob Storage
+    const fileUrl = `https://getgoods.blob.core.windows.net/user-photos/${user.photo}`;
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                photo: fileUrl // Add the file URL to the response
+            }
         }
     });
 });
