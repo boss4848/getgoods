18dae5bb3512e03847420b6f7d8b12e80ea7484b
Passakorn Puttama <54370241+boss4848@users.noreply.github.com>
2023-06-06 00:10:51 +0700

    [UPDATE] set shop id middleware

--- back_end/controllers/productController.js
+++ back_end/controllers/productController.js
@@ -1,6 +1,14 @@
 const Product = require('../models/productModel');
 const factory = require('./handlerFactory');
 
+exports.setShopIds = async (req, res, next) => {
+    // Allow nested routes
+    console.log(req.params);
+    if (!req.body.shop) req.body.shop = req.params.shopId;
+    console.log(req.body.shop);
+    next();
+};
+
 exports.getAllProducts = factory.getAll(Product);
 exports.getProduct = factory.getOne(Product, { path: 'reviews' });
 exports.updateProduct = factory.updateOne(Product);
