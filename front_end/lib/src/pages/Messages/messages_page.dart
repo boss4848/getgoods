a277c7ea204d4c5986731ea942e9e5bae59a9c74
Passakorn Puttama <54370241+boss4848@users.noreply.github.com>
2023-06-08 00:16:10 +0700

    [CREATE] messages page

---
+++ front_end/lib/src/pages/Messages/messages_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MessagesPage extends StatelessWidget {
+  const MessagesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("Messages Page"));
+  }
+}
