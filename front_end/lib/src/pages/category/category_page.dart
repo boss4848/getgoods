dda4e5cd5e16ba6e0217722d33b350e5ba3baf25
Passakorn Puttama <54370241+boss4848@users.noreply.github.com>
2023-06-08 00:15:19 +0700

    [CREAET] category page

---
+++ front_end/lib/src/pages/category/category_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CategoryPage extends StatelessWidget {
+  const CategoryPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("Category Page"));
+  }
+}
