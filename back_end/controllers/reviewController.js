ad6acd64d1be54bd6d4d21b60bf8fd82bfedc34f
Passakorn Puttama <54370241+boss4848@users.noreply.github.com>
2023-06-09 17:27:17 +0700

    [FIX] populate user

--- back_end/controllers/reviewController.js
+++ back_end/controllers/reviewController.js
@@ -1,6 +1,8 @@
 const Review = require('../models/reviewModel');
+const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
-
+const APIFeatures = require('../utils/apiFeatures');
+const { options } = require('../app');
 
 exports.setProductUserIds = (req, res, next) => {
     //Allow nested routes
@@ -9,7 +11,40 @@ exports.setProductUserIds = (req, res, next) => {
     if (!req.body.user) req.body.user = req.user.id;
     next();
 };
-exports.getAllReviews = factory.getAll(Review);
+exports.getAllReviews = catchAsync(async (req, res, next) => {
+    //Allow nested routes
+    let filter = {};
+    if (req.params.productId) filter = { product: req.params.productId };
+
+    const features = new APIFeatures(Review.find(filter), req.query)
+        .filter()
+        .sort()
+        .limitFields()
+        .paginate();
+
+    //Execute query
+    const reviews = await features.query.populate(
+        'user',
+        'name photo'
+    );
+    // https://getgoods.blob.core.windows.net/user-photos/
+    const filesUrl = reviews.map((review) => {
+        return `https://getgoods.blob.core.windows.net/user-photos/${review.user.photo}`;
+    });
+    // Replace the imageCover property with the file URL
+    reviews.forEach((review, i) => {
+        review.user.photo = filesUrl[i];
+    });
+
+    res.status(200).json({
+        status: 'success',
+        results: reviews.length,
+        data: {
+            reviews
+        }
+    });
+})
+
 exports.getReview = factory.getOne(Review);
 exports.createReview = factory.createOne(Review);
 exports.deleteReview = factory.deleteOne(Review);
