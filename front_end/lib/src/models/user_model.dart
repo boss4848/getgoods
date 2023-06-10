f12ac7bd4c3da4ec2e73eadd4a32b12b2bfc9ea8
Passakorn Puttama <54370241+boss4848@users.noreply.github.com>
2023-06-10 23:26:56 +0700

    [ADD] user detail model

--- front_end/lib/src/models/user_model.dart
+++ front_end/lib/src/models/user_model.dart
@@ -22,3 +22,43 @@ class User {
     );
   }
 }
+
+class UserDetail {
+  final String photo;
+  final String name;
+  final String email;
+  final String phone;
+  final String address;
+  final String shop;
+
+  UserDetail({
+    required this.photo,
+    required this.name,
+    required this.email,
+    required this.phone,
+    required this.address,
+    required this.shop,
+  });
+
+  factory UserDetail.fromJson(Map<String, dynamic> json) {
+    return UserDetail(
+      photo: json['photo'] ?? '',
+      name: json['name'] ?? '',
+      email: json['email'] ?? '',
+      phone: json['phone'] ?? '',
+      address: json['address'] ?? '',
+      shop: json['shop'] ?? '',
+    );
+  }
+
+  factory UserDetail.empty() {
+    return UserDetail(
+      photo: '',
+      name: '',
+      email: '',
+      phone: '',
+      address: '',
+      shop: '',
+    );
+  }
+}
