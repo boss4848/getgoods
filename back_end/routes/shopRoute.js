674c3644547d5211ff794c31ed0cce9df674bdde
Passakorn Puttama <54370241+boss4848@users.noreply.github.com>
2023-06-13 12:20:16 +0700

    [UPDATE] check if shop exists

--- back_end/routes/shopRoute.js
+++ back_end/routes/shopRoute.js
@@ -16,6 +16,7 @@ router
     .post(
         authController.protect,
         shopController.setShopUserIds,
+        shopController.checkIfShopExists,
         shopController.createShop
     );
 // Routes
