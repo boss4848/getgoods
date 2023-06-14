8e9e752e73de60f62872a6c772231ca341c04e7c
Passakorn Puttama <54370241+boss4848@users.noreply.github.com>
2023-06-14 11:47:41 +0700

    [UPDATE] add base url

--- front_end/lib/src/viewmodels/address_viewmodel.dart
+++ front_end/lib/src/viewmodels/address_viewmodel.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 import 'dart:developer';
 
 import 'package:dio/dio.dart';
-import 'package:getgoods/src/constants/constants.dart';
 import 'package:getgoods/src/models/district_model.dart';
 import 'package:getgoods/src/models/province_model.dart';
 import 'package:getgoods/src/models/sub_district_model.dart';
@@ -15,111 +14,69 @@ enum AddressState {
 
 class AddressViewModel {
   final Dio _dio = Dio();
+  final String _baseUrl = 'https://www.androidthai.in.th/flutter';
 
   List<Province> provinces = [];
   List<District> districts = [];
   List<SubDistrict> subDistricts = [];
   AddressState state = AddressState.loading;
 
-  Future<void> fetchProvinces() async {
-    state = AddressState.loading;
-
+  Future<void> _fetchData(String url, Function onSuccess) async {
     try {
-      final response = await _dio
-          .get('https://www.androidthai.in.th/flutter/getAllprovinces.php');
-      // log(response.toString());
+      final response = await _dio.get(url);
 
       if (response.statusCode == 200) {
-        final data = jsonDecode(response.data); // Parsing string to JSON
+        final data = jsonDecode(response.data);
 
         if (data is List) {
-          final List<dynamic> responseProvinces = data;
-
-          provinces = responseProvinces
-              .map((province) => Province.fromJson(province))
-              .toList();
-
+          onSuccess(data);
           state = AddressState.success;
         } else {
-          // If parsed data is not a list, set state to error
           state = AddressState.error;
         }
       } else {
-        // If status code is not 200, set state to error
         state = AddressState.error;
       }
     } catch (e) {
-      // If any exception occurs, set state to error
       state = AddressState.error;
-      log('Exception while fetching provinces: $e');
+      log('Exception while fetching data: $e');
     }
   }
 
-  Future<void> fetchDistricts(String proviceId) async {
+  void _onProvinceDataReceived(List<dynamic> data) {
+    provinces = data.map((province) => Province.fromJson(province)).toList();
+  }
+
+  void _onDistrictDataReceived(List<dynamic> data) {
+    districts = data.map((district) => District.fromJson(district)).toList();
+  }
+
+  void _onSubDistrictDataReceived(List<dynamic> data) {
+    subDistricts =
+        data.map((subDistrict) => SubDistrict.fromJson(subDistrict)).toList();
+  }
+
+  Future<void> fetchProvinces() async {
     state = AddressState.loading;
+    await _fetchData(
+      '$_baseUrl/getAllprovinces.php',
+      _onProvinceDataReceived,
+    );
+  }
 
-    try {
-      final response = await _dio.get(
-          'https://www.androidthai.in.th/flutter/getAmpByProvince.php?isAdd=true&province_id=$proviceId');
-      log(response.toString());
-
-      if (response.statusCode == 200) {
-        final data = jsonDecode(response.data); // Parsing string to JSON
-
-        if (data is List) {
-          final List<dynamic> responseDistricts = data;
-
-          districts = responseDistricts
-              .map((province) => District.fromJson(province))
-              .toList();
-
-          state = AddressState.success;
-        } else {
-          // If parsed data is not a list, set state to error
-          state = AddressState.error;
-        }
-      } else {
-        // If status code is not 200, set state to error
-        state = AddressState.error;
-      }
-    } catch (e) {
-      // If any exception occurs, set state to error
-      state = AddressState.error;
-      log('Exception while fetching provinces: $e');
-    }
+  Future<void> fetchDistricts(String provinceId) async {
+    state = AddressState.loading;
+    await _fetchData(
+      '$_baseUrl/getAmpByProvince.php?isAdd=true&province_id=$provinceId',
+      _onDistrictDataReceived,
+    );
   }
 
   Future<void> fetchSubDistricts(String districtId) async {
     state = AddressState.loading;
-
-    try {
-      final response = await _dio.get(
-          'https://www.androidthai.in.th/flutter/getDistriceByAmphure.php?isAdd=true&amphure_id=$districtId');
-      log(response.toString());
-
-      if (response.statusCode == 200) {
-        final data = jsonDecode(response.data); // Parsing string to JSON
-
-        if (data is List) {
-          final List<dynamic> responseSubDistricts = data;
-
-          subDistricts = responseSubDistricts
-              .map((province) => SubDistrict.fromJson(province))
-              .toList();
-
-          state = AddressState.success;
-        } else {
-          // If parsed data is not a list, set state to error
-          state = AddressState.error;
-        }
-      } else {
-        // If status code is not 200, set state to error
-        state = AddressState.error;
-      }
-    } catch (e) {
-      // If any exception occurs, set state to error
-      state = AddressState.error;
-      log('Exception while fetching provinces: $e');
-    }
+    await _fetchData(
+      '$_baseUrl/getDistriceByAmphure.php?isAdd=true&amphure_id=$districtId',
+      _onSubDistrictDataReceived,
+    );
   }
 }
