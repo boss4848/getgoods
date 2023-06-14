39d78cb0e1fee27021b7b02a61c89258505d6d4f
Passakorn Puttama <54370241+boss4848@users.noreply.github.com>
2023-06-14 11:47:52 +0700

    [ADD] category view model

---
+++ front_end/lib/src/viewmodels/category_viewmodel.dart
@@ -0,0 +1,9 @@
+class CategoryViewModel {
+  List<String> get categories => _categories;
+  final List<String> _categories = [
+    'processed',
+    'otop',
+    'medicinal plant',
+    'dried good',
+  ];
+}
