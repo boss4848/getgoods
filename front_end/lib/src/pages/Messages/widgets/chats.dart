d2e6295c6d2e5a7aa756e6a9bbbe4d1862e539e2
Passakorn Puttama <54370241+boss4848@users.noreply.github.com>
2023-06-16 20:47:54 +0700

    [UPDATE] chat UI

--- front_end/lib/src/pages/Messages/widgets/chats.dart
+++ front_end/lib/src/pages/Messages/widgets/chats.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:getgoods/src/constants/colors.dart';
+
+import 'chat_room.dart';
 
 class Chats extends StatelessWidget {
   const Chats({super.key});
@@ -6,9 +9,99 @@ class Chats extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.white,
-      child: const Center(
-        child: Text('Chats'),
+      color: secondaryBGColor,
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(
+            top: 62,
+            left: 12,
+            right: 12,
+          ),
+          child: Column(
+            children: List.generate(
+              5,
+              (index) => _buildChatItem(
+                context: context,
+                avatar: 'https://i.pravatar.cc/150?img=$index',
+                name: 'John Doe',
+                message: 'Hello, how are you?',
+                time: '12:00 PM',
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  GestureDetector _buildChatItem({
+    required String avatar,
+    required String name,
+    required String message,
+    required String time,
+    context,
+  }) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const ChatRoom(),
+          ),
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.all(12),
+        margin: const EdgeInsets.only(bottom: 10),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.black26,
+              blurRadius: 1.5,
+              spreadRadius: 0.5,
+            )
+          ],
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CircleAvatar(
+              backgroundImage: NetworkImage(
+                avatar,
+              ),
+              radius: 30,
+              backgroundColor: Colors.grey,
+            ),
+            const SizedBox(width: 12),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  name,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  message,
+                  style: const TextStyle(
+                    color: secondaryTextColor,
+                  ),
+                ),
+              ],
+            ),
+            const Spacer(),
+            Text(
+              time,
+              style: const TextStyle(
+                color: secondaryTextColor,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
